e1e9cc87aa1350c0716e9b9c74817a78e81de12a
chansky6 <78712443+chansky6@users.noreply.github.com>
2024-04-18 23:16:21 +0800

    添加flickr8k数据集划分

--- create_input_files.py
+++ create_input_files.py
@@ -3,7 +3,7 @@ from utils import create_input_files
 if __name__ == '__main__':
     # Create input files (along with word map)
     create_input_files(dataset='flickr8k',
-                       karpathy_json_path='../caption data/dataset_flickr8k.json',
+                       karpathy_json_path='dataset_flickr8k.json',
                        image_folder='/kaggle/input/flickr8k/Images/',
                        captions_per_image=5,
                        min_word_freq=5,
